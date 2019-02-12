b52b58ec6c78066398b80b84f7437e5c9eacd4c2
bcsj <bcsj@dtu.dk>
2019-02-12 11:47:32 +0100

    fixed whitespace, moved token to separate file

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 
+from bot_token import token
 import untamed
 import difflib
 
@@ -41,7 +42,7 @@ async def card(ctx, *name : str):
     if closest not in untamed.cards.keys():
         msg = 'Could not find card "' + name + '".' + (' Did you mean\n' + '\n'.join(closest) if len(closest) > 0 else '')
     else:
-		card = untamed.cards[closest]
+        card = untamed.cards[closest]
         msg = card['name'] + ', ' + card['faction'] + ' faction' + '\n' \
               + card['type'] + (', cost: ' + str(card['cost']) if card['cost'] is not None else '') + '\n' \
               + ('Power: ' + str(card['power']) + ', ' if card['power'] is not None else '') \
@@ -49,4 +50,4 @@ async def card(ctx, *name : str):
               + card['text']        
     await ctx.send(msg)
 
-bot.run('token')
+bot.run(token)
